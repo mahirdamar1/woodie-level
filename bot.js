5e0ec5836c17ad13262b6f3aaa390dbcea63190e
barbarbar338 <35371155+barbarbar338@users.noreply.github.com>
2018-06-09 14:42:55 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,179 @@
+﻿console.log("[App] Başlatılıyor...");
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const bot = new Discord.Client({fetchAllMembers:true});
+const ayarlar = require('./ayarlar.json');
+const ownerID = '331846231514939392'
+const chalk = require('chalk');
+const botconfig = require("./botconfig");
+const request = require("superagent")
+const mysql = require("mysql");
+const async = require("async-mysql");
+const fs = require('fs');
+const snekfetch = require('snekfetch');
+const inspect = require('util');
+const moment = require('moment');
+require('./util/eventLoader')(client);
+let xp = require("./xp.json");
+let userData = require("./userData.json")
+let purple = botconfig.purple;
+
+var prefix = ayarlar.prefix;
+
+
+
+
+const log = message => {
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${message}`);
+};
+
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+fs.readdir('./komutlar/', (err, files) => {
+  if (err) console.error(err);
+  log(`${files.length} komut yüklenecek.`);
+  files.forEach(f => {
+    let props = require(`./komutlar/${f}`);
+    log(`Yüklenen komut: ${props.help.name}.`);
+    client.commands.set(props.help.name, props);
+    props.conf.aliases.forEach(alias => {
+      client.aliases.set(alias, props.help.name);
+    });
+  });
+});
+
+client.reload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./komutlar/${command}`)];
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e){
+      reject(e);
+    }
+  });
+};
+
+client.load = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e){
+      reject(e);
+    }
+  });
+};
+
+client.unload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./komutlar/${command}`)];
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      resolve();
+    } catch (e){
+      reject(e);
+    }
+  });
+};
+
+
+
+
+client.on('message', message => {
+
+
+	if (message.author.id === client.user.id) return;
+	  console.log(`LOG: S: ${message.guild.name} M: ${message.content} Y: ${message.author.tag}`);
+
+  let xpAdd = Math.floor(Math.random() * 7) + 8;
+  console.log(xpAdd);
+
+  if(!xp[message.author.id]){
+    xp[message.author.id] = {
+      xp: 0,
+      level: 1
+    };
+  }
+
+
+  let curxp = xp[message.author.id].xp;
+  let curlvl = xp[message.author.id].level;
+  let nxtLvl = xp[message.author.id].level * 300;
+  xp[message.author.id].xp =  curxp + xpAdd;
+  if(nxtLvl <= xp[message.author.id].xp){
+    xp[message.author.id].level = curlvl + 1;
+    let lvlup = new Discord.RichEmbed()
+    .setTitle("SEVİYE ATLADIN!")
+	.setAuthor(message.author.tag)
+    .setColor(purple)
+    .addField("Şuanki seviyen:", curlvl + 1);
+
+    message.channel.send(lvlup).then(msg => {msg.delete(5000)});
+  }
+  fs.writeFile("./xp.json", JSON.stringify(xp), (err) => {
+    if(err) console.log(err)
+  });
+    
+
+    if (!userData[message.author.id]) userData[message.author.id] = {
+      messagesSent: 0
+    }
+    userData[message.author.id].messagesSent++;
+  
+    fs.writeFile('./userData.json', JSON.stringify(userData), (err) => {
+      if (err) console.error(err)
+    });
+    
+});
+
+
+
+
+
+
+
+
+client.elevation = message => {
+  if(!message.guild) {
+	return; }
+  let permlvl = 0;
+  if (message.member.hasPermission("BAN_MEMBERS")) permlvl = 2;
+  if (message.member.hasPermission("ADMINISTRATOR")) permlvl = 3;
+  if (message.author.id === ayarlar.sahip) permlvl = 4;
+  return permlvl;
+};
+
+var regToken = /[\w\d]{24}\.[\w\d]{6}\.[\w\d-_]{27}/g;
+
+client.on('warn', e => {
+  console.log(chalk.bgYellow(e.replace(regToken, 'that was redacted')));
+});
+
+client.on('error', e => {
+  console.log(chalk.bgRed(e.replace(regToken, 'that was redacted')));
+});
+
+//Bot login.
+bot.login(process.env.BOT_TOKEN).catch((err) => console.log(`[Client] Bağlantı başarısız: ${err.message}`))
+//Saves endless looking around if there is an Uncaught Promise Error.
+process.on("unhandledRejection", err => {
+  console.error("Uncaught Promise Error: \n" + err.stack);
+});
+client.login('NDQyMzgwNzkwNTQyNjMwOTEy.Dc9-zw.q4TDPaV__XsCBnFHqBISE7tmf1w');
